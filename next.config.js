3b5bcf8b5f31c27e05b723688ebfa527221bb921
Stefano Righini <stefano.righini@quinck.io>
2023-10-03 15:09:09 +0200

    add build stpes in github actions pipeline

--- next.config.js
+++ next.config.js
@@ -1,19 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  /**
-   * Set base path. This is usually the slug of your repository.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-   */
   basePath: "/futura-next-build-deploy",
-
-  /**
-   * Disable server-based image optimization. Next.js does not support
-   * dynamic features with static exports.
-   *
-   * @see https://nextjs.org/docs/pages/api-reference/components/image#unoptimized
-   */
   images: {
     unoptimized: true,
   },
